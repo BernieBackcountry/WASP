4994ce1f33af66be0f0dccc64c74c4fc0bfc426c
lexi.denhard <lexi.denhard@linquest.com>
2022-11-06 13:29:43 -0700

    linked 309 satellites

--- scraper/prepare.py
+++ scraper/prepare.py
@@ -2,8 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import pickle
 
-import utilities as utilities
-from utilities.prepare_utilities import save_satellite_info
+import scraper.utilities as utilities
 
 path1 = utilities.get_project_path().joinpath('data')
 path2 = utilities.get_project_path().joinpath('data', 'images')
@@ -14,20 +13,8 @@ celestrak_text = requests.get("https://celestrak.com/NORAD/elements/geo.txt").te
 celestrak_info = utilities.prepare_text(celestrak_text)
 utilities.save_text(path1, celestrak_info)
 
-# with open(path / 'celestrak.pkl', 'rb') as f:
-#     loaded_dict = pickle.load(f)
 
-# print(loaded_dict)
-
+# # Scrap Satbeam data
 satbeam_text = requests.get('https://satbeams.com/satellites?status=active').text
 soup = BeautifulSoup(satbeam_text, "html.parser")
-res = utilities.run_threads(soup, {"User-Agent": "Chrome/51.0.2704.103",}, path2)
-
-print("DONE")
-
-save_satellite_info(res, path1)
-
-with open(path1 / 'satbeam.pkl', 'rb') as f:
-    loaded_dict = pickle.load(f)
-
-print(loaded_dict)
+utilities.run_threads(soup, {"User-Agent": "Chrome/51.0.2704.103",}, path1, path2)
--- scraper/utilities/prepare_utilities.py
+++ scraper/utilities/prepare_utilities.py
@@ -1,4 +1,5 @@
 import queue
+import pandas as pd
 from bs4 import BeautifulSoup
 import re
 import requests
@@ -7,21 +8,38 @@ import pickle
 from tqdm import tqdm
 from pathlib import Path
 
+import scraper.utilities as utilities 
 
 # CelesTrak Functions
 def prepare_text(text: list) -> dict:
     tle_dict = {}
+    sat = []
+    sat_extra = []
+    tle = []
     for i, line in enumerate(text):
         if (i % 3 == 0) and (i <= len(text)-3):
             tle_1 = text[i+1].strip().replace(" ", "*")
             tle_2 = text[i+2].strip().replace(" ", "*")
-            tle_dict[line.strip()] = tle_1 + "\n" + tle_2
+            sat_temp = line.strip().replace("-", " ")
+            if "(" in sat_temp:
+                temp = sat_temp.split("(", 1)
+                sat_id = temp[0].upper().strip()
+                sat.append(sat_id)
+                temp_2 = temp[1]
+                sat_extra.append(temp_2[:-1])
+            else:
+                sat_extra.append("")
+                sat.append(sat_temp)
+            tle.append(tle_1 + "\n" + tle_2)
+    tle_dict = {'Satellite': sat, 
+                'Extra': sat_extra,
+                'Telemetry': tle}
     return tle_dict
 
 
 def save_text(path: Path, dict_: dict):
-    with open(path / 'celestrak.pkl', 'wb') as f:
-        pickle.dump(dict_, f)
+    df = pd.DataFrame(dict_)
+    df.to_csv(path / 'celestrak.csv', index=False)
 
 
 # Satbeam Functions
@@ -32,35 +50,37 @@ def get_all_urls(soup: BeautifulSoup, headers: dict) -> list:
     return urls
 
 
-def run_threads(soup: BeautifulSoup, headers: dict, path: Path):
+def run_threads(soup: BeautifulSoup, headers: dict, path1: Path, path2: Path):
     thread_results = []
     q = queue.Queue()
     all_urls = get_all_urls(soup, headers)
     all_urls = all_urls
-    threads = [threading.Thread(target=fetch_url, args=(url, headers, path, q)) for url in all_urls]
+    threads = [threading.Thread(target=fetch_url, args=(url, headers, path2, q)) for url in all_urls]
     for thread in threads:
         thread.start()
         response = q.get()
         thread_results.append(response)
     for thread in threads:
         thread.join()
-    return thread_results
-
+    save_satellite_info(thread_results, path1)
+    
 
 def fetch_url(url, headers, path, q):
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, timeout=20)
     # Check if the status_code is 200
     if response.status_code == 200:    
         # Parse the HTML content of the webpage
         soup = BeautifulSoup(response.content, 'html.parser')
         # Scrap satellite info
-        sat_id, pos, norad = get_satellite_info(soup)
+        sat_id, sat_extra, pos, norad = get_satellite_info(soup)
         # Put satellite info on queue
-        q.put([sat_id, pos, norad])
-        # Scrap images info
-        images = get_satellite_footprints(soup)
-        # Save images 
-        save_satellite_footprints(sat_id, path, images)
+        q.put([sat_id, sat_extra, pos, norad])
+        # Create image directories
+        # create_image_directories(sat_id, path)
+        # # Scrap images info
+        # images = get_satellite_footprints(soup)
+        # # Save images 
+        # save_satellite_footprints(sat_id, path, images)
 
 
 def find_by_label(soup: BeautifulSoup, label: str) -> str:
@@ -74,14 +94,22 @@ def find_by_next(soup: BeautifulSoup, label: str, tag: str) -> str:
 
 def get_satellite_info(soup: BeautifulSoup) -> str:
     sat = find_by_label(soup, "Satellite Name:")
+    sat = sat.replace("-", " ")
     if "(" in sat:
-        temp = sat.split("(", 1)[0]
-        sat_id = temp.upper().strip()
+        temp = sat.split("(", 1)
+        sat_id = temp[0].upper().strip()
+        temp_2 = temp[1]
+        sat_extra = temp_2[:-1]
     else:
         sat_id = sat.upper().strip()
+        sat_extra = ""
     position = str(find_by_label(soup, "Position:"))
     norad = str(find_by_next(soup, "NORAD:", "a").contents[0])
-    return sat_id, position, norad
+    return sat_id, sat_extra, position, norad
+    
+
+def create_image_directories(satellite: str, path: Path):
+    utilities.create_directory(path.joinpath(satellite))
 
 
 def get_satellite_footprints(soup: BeautifulSoup) -> list:
@@ -99,12 +127,12 @@ def get_satellite_footprints(soup: BeautifulSoup) -> list:
 
 
 def save_satellite_footprints(sat_id: str, path: Path, images: list):
+    path = path.joinpath(sat_id)
     # download and save images 
     for image in tqdm(images):
         file_name = image.split('/')[-1]
-        file_name = sat_id + "-" + file_name
         try:
-            r = requests.get(image, stream=True)
+            r = requests.get(image, stream=True, timeout=20)
             if r.status_code == 200:
                 with open(path / file_name, 'wb') as f:
                     for chunk in r:
@@ -113,15 +141,19 @@ def save_satellite_footprints(sat_id: str, path: Path, images: list):
             pass    
 
 
-def list_to_dict(output) -> dict:
-    dict_ = {}
-    for ele in output:
-        for e in ele:
-            dict_[e[0]] = e[1] + e[2]
-    return dict_
-
-
-def save_satellite_info(output, path: Path):
-    dict_ = list_to_dict(output)
-    with open(path / 'satbeam.pkl', 'wb') as f:
-        pickle.dump(dict_, f)
+def save_satellite_info(results: list, path: Path):
+    sat = []
+    sat_extra = []
+    pos = []
+    nor = []
+    for ele in results:
+        sat.append(ele[0])
+        sat_extra.append(ele[1])
+        pos.append(ele[2])
+        nor.append(ele[3])
+    dict_ = {'Satellite': sat,
+            'Extra': sat_extra,
+            'Position': pos,
+            'NORAD': nor}
+    df = pd.DataFrame(dict_)
+    df.to_csv(path / 'satbeam.csv', index=False)
