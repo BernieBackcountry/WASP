aee03f8cfeda8ba95b96e3d9d33e9a1aa6da3d23
Lexi Denhard <lexi.denhard@linquest.com>
2023-01-16 17:07:45 -0700

    fix lyngsat save functions and file types

--- wasp_tool/prepare.py
+++ wasp_tool/prepare.py
@@ -4,27 +4,30 @@ import wasp_tool.utilities as utilities
 # create data directory and sub-directories
 utilities.create_directory(utilities.get_project_path().joinpath('data', 'images'))
 utilities.create_directory(utilities.get_project_path().joinpath('data', 'pdfs'))
+utilities.create_directory(utilities.get_project_path().joinpath('data', 'tables'))
 
 path_data = utilities.get_project_path().joinpath('data')
 path_images = utilities.get_project_path().joinpath('data', 'images')
 path_pdfs = utilities.get_project_path().joinpath('data', 'pdfs')
+path_tables = utilities.get_project_path().joinpath('data', 'tables')
 
 # Scrap Celestrak data
-# celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
-# utilities.save_dict_to_csv(path_data, celestrak_data, "celestrak.csv")
-# print("Celestrak DONE")
+celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
+utilities.save_dict_to_csv(path_data, celestrak_data, "celestrak.csv")
+print("Celestrak DONE")
 
-# # Scrap Satbeams data
-# satbeams_data, satbeams_footprints = utilities.prepare_satbeams('https://satbeams.com/satellites?status=active')
-# utilities.save_dict_to_csv(path_data, satbeams_data, "satbeams.csv")
-# print(satbeams_footprints)
-# utilities.save_footprints(path_images, satbeams_data["Satellite"], satbeams_footprints)
-# print("Satbeams DONE")
+# Scrap Satbeams data
+satbeams_data, satbeams_footprints = utilities.prepare_satbeams('https://satbeams.com/satellites?status=active')
+utilities.save_dict_to_csv(path_data, satbeams_data, "satbeams.csv")
+print(satbeams_footprints)
+utilities.save_footprints(path_images, satbeams_data["Satellite"], satbeams_footprints)
+print("Satbeams DONE")
 
-# # Scrap Lyngsat data
-# lyngsat_data = utilities.prepare_lyngsat('https://www.lyngsat.com/')
-# utilities.save_as_pickle(path_data, lyngsat_data, "lyngsat.pickle")
-# print("Lyngsat DONE")
+# Scrap Lyngsat data
+lyngsat_data = utilities.prepare_lyngsat('https://www.lyngsat.com/')
+utilities.save_dict_to_csv(path_data, {"Satellite": list(lyngsat_data.keys())}, "lyngsat.csv")
+utilities.save_tables(path_tables, lyngsat_data)
+print("Lyngsat DONE")
 
 # Scrap Altervista data
 altervista_data, altervista_pdfs = utilities.prepare_altervista('http://frequencyplansatellites.altervista.org/')
--- wasp_tool/utilities/save_utilities.py
+++ wasp_tool/utilities/save_utilities.py
@@ -3,18 +3,13 @@ from pathlib import Path
 import threading
 from tqdm import tqdm
 import requests
-import json
 
 import wasp_tool.utilities as utilities
+
 
 def save_dict_to_csv(path: Path, dict_: dict, file_name: str):
     df = pd.DataFrame(dict_)
     df.to_csv(path / file_name, index=False)
-
-
-def save_as_pickle(path: Path, dict_: dict, file_name: str):
-    with open(path / "w") as outfile:
-        json.dump(dict_, outfile)
 
 
 def save_footprints(path: Path, sat_names: list, footprints: list):
@@ -42,6 +37,18 @@ def image_download(path: Path, sat_name: str, image_links: list, image_titles: l
                         f.write(chunk)
         except Exception as e:
             pass 
+
+
+def save_tables(path: Path, dict_: dict):
+    for key, lst in dict_.items():
+        # \ causes problem with directory name
+        if "/" in key:
+            key = key.replace("/", "-")
+        utilities.create_directory(path.joinpath(key))
+        file_path = path.joinpath(key)
+        for i, ele in enumerate(lst): 
+            file_name = key + "_" + str(i) + ".csv"
+            ele.to_csv(file_path / file_name, index=False)
 
 
 def save_pdfs(path: Path, names: list, urls: list):
