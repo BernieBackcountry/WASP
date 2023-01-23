ae66220043ed25c3a2a9294b601eec3ef23338dc
Lexi Denhard <lexi.denhard@linquest.com>
2023-01-23 15:57:39 -0700

    Add standardization to satellite naming across sites

---
+++ wasp_tool/utilities/prepare_utilities.py
@@ -0,0 +1,129 @@
+import pandas as pd
+from pathlib import Path
+import threading
+from tqdm import tqdm
+import requests
+
+import wasp_tool.utilities as utilities
+
+
+def standardize_satellite(sat_name: str) -> str:
+    # strip end parenthesis 
+    name_replace = sat_name.replace(")", "")
+    # strip leading/following spaces
+    name_strip = name_replace.strip()
+    # cast whole string to upper
+    name_upper = name_strip.upper()
+    
+    # CASE 1: replacing sub-strings for consistency
+    case_1 = {"G-SAT": "GSAT", 
+        "HELLASSAT": "HELLAS SAT", 
+        "HELLAS-SAT": "HELLAS SAT", 
+        "HOTBIRD": "HOT BIRD"}
+    for substring in case_1.keys():
+        if substring in name_upper:
+            name_upper = name_upper.replace(substring, case_1[substring])
+            break
+    
+    # CASE 2: replacing white space with dashes
+    dash_add = ["ABS", "AMC", "AMOS", "ARABSAT", "ATHENA FIDUS", "BADR", "BSAT", "BEIDOU ", 
+        "BULGARIASAT", "CIEL", "CMS", "EXPRESS AT", "EXPRESS AM", "GSAT", "HORIZONS", "INSAT", 
+        "JCSAT", "KAZSAT", "MEASAT", "NSS", "PAKSAT", "PSN", "SES", "TKSAT", "VIASAT", "VINASAT", 
+        "WILDBLUE", "XM "]
+    if (any(substring in name_upper for substring in dash_add)) and ("SERIES" not in name_upper):
+        name_upper = name_upper.replace(" ", "-", 1)
+    
+    # CASE 3: replacing dashes with white space
+    dash_remove = ["EXPRESS AMU", "THURAYA"]
+    if any(substring in name_upper for substring in dash_remove):
+        name_upper = name_upper.replace("-", " ", 1)
+
+    new_name = name_upper
+    return new_name
+
+
+# # altervista 
+# def get_pdf_name(pdf: str):  
+#     ele = pdf.split("/")[-1]
+#     ele = ele.replace(".pdf", "")
+#     ele = ele.replace("_", " ")
+#     pdf_name = ele.strip()
+#     return pdf_name
+
+# # celestrak
+#             sat = line.strip()
+#             # strip excess sat names
+#             if "(" in sat:
+#                 temp = sat.split("(", 1)
+#                 sat_id = temp[0].strip()
+#                 sat_name.append(sat_id)
+#                 temp_2 = temp[1].strip()
+#                 sat_name_excess.append(temp_2[:-1])
+#             else:
+#                 sat_name.append(sat)
+#                 sat_name_excess.append("")
+                
+#             tle.append(tle_1 + "\n" + tle_2)
+    
+#     tle_dict = {'Satellite': sat_name, 
+#                 'Extra Names': sat_name_excess,
+#                 'TLE': tle}
+
+def save_dict_to_csv(path: Path, dict_: dict, file_name: str):
+    df = pd.DataFrame(dict_)
+    df.to_csv(path / file_name, index=False)
+
+
+def save_footprints(path: Path, sat_names: list, footprints: list):
+    images, titles = map(list, zip(*footprints))
+    threads = [threading.Thread(target=image_download, args=(path, sat, images, titles, k)) for k, sat in enumerate(sat_names)]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    
+def image_download(path: Path, sat_name: str, image_links: list, image_titles: list, iter: int):
+    utilities.create_directory(path.joinpath(sat_name))
+    path = path.joinpath(sat_name)
+    sat_images = image_links[iter]
+    sat_titles = image_titles[iter]
+    # download and save images 
+    for i, image in tqdm(enumerate(sat_images)):
+        file_name = sat_titles[i] + ".jpg"
+        try:
+            r = requests.get(image, stream=True, timeout=20)
+            if r.status_code == 200:
+                with open(path / file_name, 'wb') as f:
+                    for chunk in r:
+                        f.write(chunk)
+        except Exception as e:
+            pass 
+
+
+def save_tables(path: Path, dict_: dict):
+    for key, lst in dict_.items():
+        # \ causes problem with directory name
+        if "/" in key:
+            key = key.replace("/", "-")
+        utilities.create_directory(path.joinpath(key))
+        file_path = path.joinpath(key)
+        for i, ele in enumerate(lst): 
+            file_name = key + "_" + str(i) + ".csv"
+            ele.to_csv(file_path / file_name, index=False)
+
+
+def save_pdfs(path: Path, names: list, urls: list):
+    for i, url in enumerate(urls):
+        sat_name = names[i]
+        utilities.create_directory(path.joinpath(sat_name))
+        file_path = path.joinpath(sat_name)
+        try:
+            req = requests.get(url)
+            file_name = sat_name + ".pdf"
+            print("File", sat_name, "downloading")
+            pdf = open(file_path / file_name, 'wb')
+            pdf.write(req.content)
+            pdf.close()
+        except Exception as e:
+            pass
