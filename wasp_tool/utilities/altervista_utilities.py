6c10136e7505ecb9df0672b298d9a805d77c0b92
Lexi Denhard <lexi.denhard@linquest.com>
2023-01-25 15:03:57 -0700

    added pri and sec columns to altervista info as well as naming schema

--- wasp_tool/utilities/altervista_utilities.py
+++ wasp_tool/utilities/altervista_utilities.py
@@ -6,13 +6,17 @@ import wasp_tool.utilities as utilities
 
 def prepare_altervista(url: str):
     sat_urls = get_urls(url)
-    pri_satNames = []
-    pdf_urls = []
+    sat_dict = {}
+    pri_satNames, sec_satNames, pdf_urls = ([] for i in range(3))
     for sat in sat_urls:
-        sat_list, url_list = get_pdf_urls(sat)
-        pri_satNames.extend(sat_list)
+        pri_sat, sec_sat, url_list = get_pdf_urls(sat)
+        pri_satNames.extend(pri_sat)
+        sec_satNames.extend(sec_sat)
         pdf_urls.extend(url_list)
-    return pri_satNames, pdf_urls
+
+    sat_dict = {"priSatName": pri_satNames,
+                "secSatName": sec_satNames}
+    return sat_dict, pdf_urls
 
 
 def get_urls(url: str) -> list:
@@ -29,20 +33,36 @@ def get_pdf_urls(url: str) -> list:
     req = requests.get(url)
     soup = BeautifulSoup(req.text, "lxml")
     sidebar = soup.find("div", id="sidebar")
-    urls = []
-    sats = []
+    priSats, secSats, urls = ([] for i in range(3)) 
     for a in sidebar.find_all('a', href=True):
         if ".pdf" in a['href']:
             urls.append(a['href'])
-            # TODOOOOOOOOOOOOOOOOOOOOOOOOOO need to add fucntionality to clean this up
             temp = a.text
-            pri_satName = utilities.standardize_satellite(temp)
-            sats.append(pri_satName)
-    return sats, urls
+            pri_sat, sec_sat = get_sats(temp)
+            pri_satName = utilities.standardize_satellite(pri_sat)
+            sec_satName = utilities.standardize_satellite(sec_sat)
+            priSats.append(pri_satName)
+            secSats.append(sec_satName)
+    return priSats, secSats, urls
 
 
-def get_pdf_name(pdf: str):  
-    ele = pdf.split("/")[-1]
-    ele = ele.replace(".pdf", "")
-    pdf_name = ele.replace("_", " ")
-    return pdf_name
+def get_sats(text: str):
+    delimiters = ["(", "-->", "-- >", "--"]
+    if any(s in text for s in delimiters):
+        min = 1000
+        for delim in delimiters:
+            if (text.find(delim) != -1) and (text.find(delim) < min):
+                min = text.find(delim)
+                split_by = delim
+        priSatName, secSatName = split_text(text, split_by)
+    else:
+        priSatName = text
+        secSatName = ""
+    return priSatName, secSatName
+
+
+def split_text(text: str, split_on: str):
+    text_split = text.split(split_on, 1)
+    pri = text_split[0]
+    sec = text_split[1]
+    return pri, sec
