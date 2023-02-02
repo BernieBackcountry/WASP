7fda5c1fb86aafd8681dc99171c70b9b293f9319
Benjamin Johnson <benjamin.johnson@contemplasolutions.com>
2023-02-02 16:41:49 -0700

    Added no timeout to gunicorn

--- wasp_tool/prepare_celestrak.py
+++ wasp_tool/prepare_celestrak.py
@@ -2,7 +2,6 @@ import wasp_tool.utilities as utilities
 
 
 path = utilities.get_project_path().joinpath('wasp_tool')
-print(path)
 
 # create data directory
 path_data = path.joinpath('data')
@@ -12,6 +11,4 @@ utilities.create_directory(path_data)
 # Scrap Celestrak data
 celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
 utilities.save_dict_to_csv(path_data, celestrak_data, "celestrak.csv")
-print(path_data.exists())
-print(path_data.joinpath("celestrak.csv").exists())
 print("CELESTRAK COMPLETE")
