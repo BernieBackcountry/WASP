471c6021b59b2d63a18aaf6906cdb108ecb9c302
Benjamin Johnson <benjamin.johnson@contemplasolutions.com>
2023-02-02 16:00:39 -0700

    More prints

--- wasp_tool/prepare_celestrak.py
+++ wasp_tool/prepare_celestrak.py
@@ -4,8 +4,10 @@ import wasp_tool.utilities as utilities
 path = utilities.get_project_path().resolve().parent.joinpath('wasp_tool')
 
 # create data directory
-utilities.create_directory(path.joinpath('data'))
 path_data = path.joinpath('data')
+print(path_data)
+utilities.create_directory(path_data)
+
 
 # Scrap Celestrak data
 celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
