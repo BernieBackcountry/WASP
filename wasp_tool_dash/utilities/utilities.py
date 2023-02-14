a88154f0bb8c43feccb48b7bce0d6e417288000a
Lexi Denhard <lexi.denhard@linquest.com>
2023-02-14 10:40:28 -0700

    add conditional function

--- wasp_tool_dash/utilities/utilities.py
+++ wasp_tool_dash/utilities/utilities.py
@@ -1,6 +1,7 @@
 import base64
 from dash import html
 from pathlib import Path
+import botocore
 
 
 def get_project_path() -> Path:
@@ -15,3 +16,11 @@ def encode_image(path: Path) -> html.Img:
 def encode_image_pdf(path: Path) -> html.Img:
     encoded_image = base64.b64encode(open(path, 'rb').read())
     return html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), width='90%')
+
+
+def prefix_exists(aws_client: botocore.client.S3, aws_bucket: str, key: str):
+    try:
+        aws_client.head_object(Bucket=aws_bucket, Key=key)
+        return True
+    except:
+        return False
