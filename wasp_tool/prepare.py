a338df02cb7e3521b87783f3f52cc52d5714a453
Lexi Denhard <lexi.denhard@linquest.com>
2023-02-15 11:04:52 -0700

    uncomment prepare file

--- wasp_tool/prepare.py
+++ wasp_tool/prepare.py
@@ -13,25 +13,25 @@ AWS_BUCKET_NAME = 'bucketeer-40b6657d-c39a-4bb2-b827-f1fe40ec93db' #os.environ.g
 
 start_time = time.time()
 
-# # Scrap Celestrak data
-# celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
-# utilities.save_dict_to_csv(AWS_BUCKET_NAME, celestrak_data, "celestrak.csv")
-# print("--- %s seconds ---" % (time.time() - start_time))
-# print("CELESTRAK COMPLETE")
+# Scrap Celestrak data
+celestrak_data = utilities.prepare_celestrak('https://celestrak.com/NORAD/elements/geo.txt')
+utilities.save_dict_to_csv(AWS_BUCKET_NAME, celestrak_data, "celestrak.csv")
+print("--- %s seconds ---" % (time.time() - start_time))
+print("CELESTRAK COMPLETE")
 
-# # Scrap Lyngsat data
-# lyngsat_data, lyngsat_tables = utilities.prepare_lyngsat('https://www.lyngsat.com/')
-# utilities.save_dict_to_csv(AWS_BUCKET_NAME, lyngsat_data, "lyngsat.csv")
-# utilities.save_tables(AWS_BUCKET_NAME, lyngsat_tables)
-# print("--- %s seconds ---" % (time.time() - start_time))
-# print("LYNGSAT COMPLETE")
+# Scrap Lyngsat data
+lyngsat_data, lyngsat_tables = utilities.prepare_lyngsat('https://www.lyngsat.com/')
+utilities.save_dict_to_csv(AWS_BUCKET_NAME, lyngsat_data, "lyngsat.csv")
+utilities.save_tables(AWS_BUCKET_NAME, lyngsat_tables)
+print("--- %s seconds ---" % (time.time() - start_time))
+print("LYNGSAT COMPLETE")
 
-# # Scrap Altervista data
-# altervista_data, altervista_pdfs = utilities.prepare_altervista('http://frequencyplansatellites.altervista.org/')
-# utilities.save_dict_to_csv(AWS_BUCKET_NAME, altervista_data, "altervista.csv")
-# utilities.save_pdfs(AWS_CLIENT, AWS_BUCKET_NAME, altervista_data['priSatName'], altervista_pdfs)
-# print("--- %s seconds ---" % (time.time() - start_time))
-# print("ALTERVISTA COMPLETE") 
+# Scrap Altervista data
+altervista_data, altervista_pdfs = utilities.prepare_altervista('http://frequencyplansatellites.altervista.org/')
+utilities.save_dict_to_csv(AWS_BUCKET_NAME, altervista_data, "altervista.csv")
+utilities.save_pdfs(AWS_CLIENT, AWS_BUCKET_NAME, altervista_data['priSatName'], altervista_pdfs)
+print("--- %s seconds ---" % (time.time() - start_time))
+print("ALTERVISTA COMPLETE") 
 
 # Scrap Satbeams data
 satbeams_data, satbeams_footprints = utilities.prepare_satbeams('https://satbeams.com/satellites?status=active')
